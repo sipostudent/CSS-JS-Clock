538ff57de8b3998baaa6aa85271d88332d62085b
Sipo Charles <sipocharles@yahoo.co.uk>
2019-09-13 23:40:41 +0100

    added function for getting minutes

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
     const now = new Date();
+    
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    hourHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
